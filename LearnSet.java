793fb267dadc0cdf3b83e2cab92717de54011cac
Suryansh <168512396+suryanshS07@users.noreply.github.com>
2025-09-16 21:07:46 +0530

    Update LearnSet.java

--- LearnSet.java
+++ LearnSet.java
@@ -6,20 +6,21 @@ import java.util.TreeSet;
 public class LearnSet {
     public static void main(String[] args) {
         Set<Integer> set = new LinkedHashSet<>();
-        //Set<Integer> set = new HashSet<>();
-        //Set<Integer> set = new TreeSet<>();
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new TreeSet<>();
         set.add(232);
         set.add(21);
         set.add(12);
         set.add(54);
         set.add(43);
         System.out.println(set);
-//       System.out.println(set.contains(100));
-//      System.out.println(set.remove(232));
-//        System.out.println(set);
-//        set.add(21);
-//        //It does not allow to add the duplicate elements in the set
-//        System.out.println(set.size());
+      System.out.println(set.contains(100));
+     System.out.println(set.remove(232));
+       System.out.println(set);
+       set.add(21);
+       //It does not allow to add the duplicate elements in the set
+  //Here Dont get Confuse after seeing the initialising the new set of the same name it just to showcase only .
+       System.out.println(set.size());
 
         }
     }
